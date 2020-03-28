d37e47c33b17fa477f54be17ead0dea1d46984a7
Ashraf Madina <ash822m@gmail.com>
2020-03-28 11:08:44 -0400

    added week 4 assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,24 @@
 
-<script>
-    $(function () {
+    $(function() {
         $(".carousel").carousel({ interval: 2000 });
-        $("#carouselButton").click(function () {
-            if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-                $(".carousel").carousel("pause");
-                $("#carouselButton").children("i").removeClass("fa-pause");
-                $("#carouselButton").children("i").addClass("fa-play");
-            } else {
-                $(".carousel").carousel("cycle");
-                $("#carouselButton").children("i").removeClass("fa-play");
-                $("#carouselButton").children("i").addClass("fa-pause");
-            }
+            $("#carouselButton").click(function() {
+                if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+        $(".carousel").carousel("pause");
+                    $("#carouselButton").children("i").removeClass("fa-pause");
+                    $("#carouselButton").children("i").addClass("fa-play");
+                } else {
+        $(".carousel").carousel("cycle");
+                    $("#carouselButton").children("i").removeClass("fa-play");
+                    $("#carouselButton").children("i").addClass("fa-pause");
+                }
+            });
         });
+
+     $("#reserveButton").click(function(){
+        $("#reserveModal").modal("show");
+     });
+
+     $("#loginButton").click(function(){
+        $("#loginModal").modal("show");
+    
     });
-</script>
