2df334871ca08f7e2823aeda7e88b6aec6fc2cb4
Ashraf Madina <ashraf.madina@asset-map.com>
2020-04-20 19:56:28 -0400

    bug fix

---
+++ JavaScript/scope.js
@@ -0,0 +1,5 @@
+var myVar = 1;
+let myLet = 2;
+const myConst = 3;
+
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,27 @@
-
-    $(function() {
-        $(".carousel").carousel({ interval: 2000 });
-            $("#carouselButton").click(function() {
-                if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-        $(".carousel").carousel("pause");
-                    $("#carouselButton").children("i").removeClass("fa-pause");
-                    $("#carouselButton").children("i").addClass("fa-play");
-                } else {
-        $(".carousel").carousel("cycle");
-                    $("#carouselButton").children("i").removeClass("fa-play");
-                    $("#carouselButton").children("i").addClass("fa-pause");
-                }
-            });
-        });
-
-     $("#reserveButton").click(function(){
-        $("#reserveModal").modal("show");
-     });
-
-     $("#loginButton").click(function(){
-        $("#loginModal").modal("show");
-    
+$(function() {
+    $(".carousel").carousel({interval:2000});
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
     });
+
+    $("#reserveButton").click(function() {
+        $("#reserveModal").modal("show");
+    });
+
+    $("#loginButton").click(function() {
+        $("#loginModal").modal("show");
+    });
+        $('[data-toggle="tooltip"]').tooltip();
+     
+   
+});
+
+    
